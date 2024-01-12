ddff5c12e6cabb1b1f03d15ee5571506116c25aa
BenjaminDerProgrammierer <benjamin.p.hartmann@gmail.com>
2024-01-12 18:09:46 +0100

    Add Auth System and Home page

---
+++ api/db/migrations/20240112165848_auth2/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the `UserExample` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "UserExample";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME,
+    "webAuthnChallenge" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "UserCredential" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" INTEGER NOT NULL,
+    "publicKey" BLOB NOT NULL,
+    "transports" TEXT,
+    "counter" BIGINT NOT NULL,
+    CONSTRAINT "UserCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_webAuthnChallenge_key" ON "User"("webAuthnChallenge");
---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,47 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+import { useAuth } from 'src/auth'
+
+const HomePage = () => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+      <header>
+        <div className="flex-between">
+          <h1>
+            <Link to={routes.home()}>Redwood Blog</Link>
+          </h1>
+          {isAuthenticated ? (
+            <div>
+              <span>Logged in as {currentUser.email}</span>{' '}
+              <button type="button" onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Login</Link>
+          )}
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
